3bee8ed969d70f69dca0fb53979cf49e71ae9bb2
Federico Stra <stra.federico@gmail.com>
2020-12-11 12:58:42 +0100

    WIP: refactor using helper functions

--- src/RedefStructs.jl
+++ src/RedefStructs.jl
@@ -1,119 +1,80 @@
 module RedefStructs
 
-export @redef, @redef_
-
-macro redefinable(struct_def)
-    struct_def isa Expr && struct_def.head == :struct || error("struct definition expected")
-    is_unionall = false
-    if struct_def.args[2] isa Symbol
-        name = struct_def.args[2]
-        real_name = struct_def.args[2] = gensym(name)
-    elseif struct_def.args[2].head == :curly
-        is_unionall = true
-        name = struct_def.args[2].args[1]
-        real_name = struct_def.args[2].args[1] = gensym(name)
-    elseif struct_def.args[2].head == :<:
-        if struct_def.args[2].args[1] isa Symbol
-            name = struct_def.args[2].args[1]
-            real_name = struct_def.args[2].args[1] = gensym(name)
-        elseif struct_def.args[2].args[1].head == :curly
-            is_unionall = true
-            name = struct_def.args[2].args[1].args[1]
-            real_name = struct_def.args[2].args[1].args[1] = gensym(name)
-        else
-            error("expected `S <: AbstractType`")
-        end
-    else
-        error("expected `S` or `S <: AbstractType`")
-    end
-    if is_unionall
-        fix_name = :($real_name.body.name.name = $(QuoteNode(name)))
-    else
-        fix_name = :($real_name.name.name = $(QuoteNode(name)))
-    end
-    esc(quote
-        $struct_def
-        $fix_name
-        $name = $real_name # this should be `const $name = $real_name`
-    end)
-end
-
-macro redef_(struct_def)
-    struct_def isa Expr && struct_def.head == :struct || error("struct definition expected")
-    @show struct_def.args[2]
-    is_unionall = false
-    if struct_def.args[2] isa Symbol
-        name = struct_def.args[2]
-        real_name = struct_def.args[2] = gensym(name)
-    elseif struct_def.args[2].head == :curly
-        is_unionall = true
-        name = struct_def.args[2].args[1]
-        real_name = struct_def.args[2].args[1] = gensym(name)
-    elseif struct_def.args[2].head == :<:
-        if struct_def.args[2].args[1] isa Symbol
-            name = struct_def.args[2].args[1]
-            real_name = struct_def.args[2].args[1] = gensym(name)
-        elseif struct_def.args[2].args[1].head == :curly
-            is_unionall = true
-            name = struct_def.args[2].args[1].args[1]
-            real_name = struct_def.args[2].args[1].args[1] = gensym(name)
-        else
-            error("expected `S <: AbstractType`")
-        end
-    else
-        error("expected `S` or `S <: AbstractType`")
-    end
-    @show name
-    @show real_name
-    if is_unionall
-        fix_name = :($real_name.body.name.name = $(QuoteNode(name)))
-    else
-        fix_name = :($real_name.name.name = $(QuoteNode(name)))
-    end
-    esc(quote
-        $struct_def
-        $fix_name
-        $name = $real_name # this should be `const $name = $real_name`
-    end)
-end
+export @redef, @redef_print
 
 macro redef(struct_def)
-    struct_def isa Expr && struct_def.head == :struct || error("struct definition expected")
-    @show struct_def.args[2]
-    is_unionall = false
-    if struct_def.args[2] isa Symbol
-        name = struct_def.args[2]
-        real_name = struct_def.args[2] = gensym(name)
-    elseif struct_def.args[2].head == :curly
-        is_unionall = true
-        name = struct_def.args[2].args[1]
-        real_name = struct_def.args[2].args[1] = gensym(name)
-    elseif struct_def.args[2].head == :<:
-        if struct_def.args[2].args[1] isa Symbol
-            name = struct_def.args[2].args[1]
-            real_name = struct_def.args[2].args[1] = gensym(name)
-        elseif struct_def.args[2].args[1].head == :curly
-            is_unionall = true
-            name = struct_def.args[2].args[1].args[1]
-            real_name = struct_def.args[2].args[1].args[1] = gensym(name)
-        else
-            error("expected `S <: AbstractType`")
-        end
-    else
-        error("expected `S` or `S <: AbstractType`")
-    end
-    @show name
-    @show real_name
-    if is_unionall
-        fix_print = :(Base.show_datatype(io::Base.IO, ::Base.Type{$real_name.body}) = Base.print(io, $(QuoteNode(name))))
-    else
-        fix_print = :(Base.show_datatype(io::Base.IO, ::Base.Type{$real_name}) = Base.print(io, $(QuoteNode(name))))
-    end
+    name = struct_def_name(struct_def)
+    real_name = gensym(name)
+    struct_def = replace!(struct_def, name, real_name)
+    fix_name = :(Base.unwrap_unionall($real_name).name.name = $(QuoteNode(name)))
+    esc(quote
+        $struct_def
+        $fix_name
+        $name = $real_name # this should be `const $name = $real_name`
+        nothing # avoid returning the new struct
+    end)
+end
+
+macro redef_print(struct_def)
+    name = struct_def_name(struct_def)
+    real_name = gensym(name)
+    struct_def = replace!(struct_def, name, real_name)
+    fix_print = :(Base.show_datatype(io::Base.IO, ::Base.Type{Base.unwrap_unionall($real_name)}) = Base.print(io, $(QuoteNode(name))))
     esc(quote
         $struct_def
         $fix_print
         $name = $real_name # this should be `const $name = $real_name`
+        nothing # avoid returning the new struct
     end)
+end
+
+"""
+    struct_def_name(struct_def)
+
+Returns the name of the `struct` being defined in a `struct ... end` block.
+"""
+function struct_def_name(struct_def)
+    struct_def isa Expr && struct_def.head == :struct || error("expected struct definition")
+    sig = struct_def.args[2]    # signature of the struct: `struct <sig> ... end`
+    if sig isa Symbol
+        sig
+    elseif sig.head == :curly
+        sig.args[1]
+    elseif sig.head == :<:      # `struct <sig> <: ... ... end`
+        sig = sig.args[1]
+        if sig isa Symbol
+            sig
+        elseif sig.head == :curly
+            sig.args[1]
+        else
+            error("invalid `<:` signature, expected `S <: AbstractType`")
+        end
+    else
+        error("expected signature `S` or `S <: AbstractType`")
+    end
+end
+
+"""
+    replace!(code::Expr, old::Symbol, new::Symbol)
+
+Replaces every appearance of `old` in `code` with `new`.
+Modifies `code` and returns it.
+
+"""
+function replace!(code::Expr, old::Symbol, new::Symbol)
+    code.head = replace!(code.head, old, new)
+    for i in eachindex(code.args)
+        code.args[i] = replace!(code.args[i], old, new)
+    end
+    code
+end
+
+function replace!(code::Symbol, old::Symbol, new::Symbol)
+    code === old ? new : code
+end
+
+function replace!(code, old::Symbol, new::Symbol)
+    code
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,27 @@ using RedefStructs
 using Test
 
 @testset "redef" begin
-    abstract type A end
-    @redef struct S end
-    @redef struct S <: A end
-    @redef struct S{T} end
-    @redef struct S{T} <: A end
+    @testset "valid" begin
+        abstract type A end
+        @redef struct S end
+        @redef struct S <: A end
+        @redef struct S{T} end
+        @redef struct S{T} <: A end
+    end
+    # @testset "invalid" begin
+    #     abstract type A end
+    #     @test_throws ErrorException @redef S
+    #     @test_throws ErrorException @redef :S
+    #     @test_throws ErrorException @redef 1+2
+    # end
 end
 
-@testset "redef_" begin
+
+
+@testset "redef_print" begin
     abstract type A end
-    @redef_ struct S end
-    @redef_ struct S <: A end
-    @redef_ struct S{T} end
-    @redef_ struct S{T} <: A end
+    @redef_print struct S end
+    @redef_print struct S <: A end
+    @redef_print struct S{T} end
+    @redef_print struct S{T} <: A end
 end
